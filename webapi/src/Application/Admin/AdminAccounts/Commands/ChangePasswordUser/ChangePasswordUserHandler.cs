50045824ddc71a702f83306392aea83119febb85
Salvador Nicolas <snicoper@gmail.com>
2020-03-26 21:44:33 +0100

    Validacion de contrasela al cambiar

--- webapi/src/Application/Admin/AdminAccounts/Commands/ChangePasswordUser/ChangePasswordUserHandler.cs
+++ webapi/src/Application/Admin/AdminAccounts/Commands/ChangePasswordUser/ChangePasswordUserHandler.cs
@@ -2,8 +2,10 @@ using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using NetClock.Application.Common.Exceptions;
+using NetClock.Application.Common.Interfaces.Validations;
 using NetClock.Domain.Entities.Identity;
 
 namespace NetClock.Application.Admin.AdminAccounts.Commands.ChangePasswordUser
@@ -11,29 +13,50 @@ namespace NetClock.Application.Admin.AdminAccounts.Commands.ChangePasswordUser
     public class ChangePasswordUserHandler : IRequestHandler<ChangePasswordUserCommand, Unit>
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly PasswordValidator<ApplicationUser> _passwordValidator;
+        private readonly IValidationFailureService _validationFailureService;
+        private readonly IStringLocalizer<ApplicationUser> _localizer;
         private readonly ILogger<ChangePasswordUserHandler> _logger;
 
         public ChangePasswordUserHandler(
             UserManager<ApplicationUser> userManager,
+            PasswordValidator<ApplicationUser> passwordValidator,
+            IValidationFailureService validationFailureService,
+            IStringLocalizer<ApplicationUser> localizer,
             ILogger<ChangePasswordUserHandler> logger)
         {
             _userManager = userManager;
+            _passwordValidator = passwordValidator;
+            _validationFailureService = validationFailureService;
+            _localizer = localizer;
             _logger = logger;
         }
 
         public async Task<Unit> Handle(ChangePasswordUserCommand request, CancellationToken cancellationToken)
         {
             _logger.LogInformation($"Usuario {request.Id} va a cambiar la contraseña");
-            var user = await _userManager.FindByIdAsync(request.Id);
+            var applicationUser = await _userManager.FindByIdAsync(request.Id);
 
-            if (user is null)
+            if (applicationUser is null)
             {
                 _logger.LogWarning($"Usuario {request.Id} intenta cambiar contraseña y no existe");
                 throw new NotFoundException(nameof(ApplicationUser), nameof(request.Id));
             }
 
-            user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, request.NewPassword);
-            await _userManager.UpdateAsync(user);
+            var validPassword =
+                await _passwordValidator.ValidateAsync(_userManager, applicationUser, request.NewPassword);
+
+            if (!validPassword.Succeeded)
+            {
+                var errorMessage = _localizer["La contraseña no es valida"];
+                _logger.LogWarning(errorMessage);
+                _validationFailureService.Add("Password", errorMessage);
+            }
+
+            applicationUser.PasswordHash =
+                _userManager.PasswordHasher.HashPassword(applicationUser, request.NewPassword);
+
+            await _userManager.UpdateAsync(applicationUser);
             _logger.LogInformation($"Usuario {request.Id} contraseña cambiada con éxito");
 
             return Unit.Value;
